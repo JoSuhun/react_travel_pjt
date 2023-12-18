14593566007174494b7f30d9457151781296d263
BMJ <peku9693@gmail.com>
2023-12-18 20:35:50 +0900

    component 및 routes 폴더 생성

---
+++ travel-service/src/routes/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TourList from "../components/Main/TourList";
+
+function Main() {
+  return (
+    <div>
+      <h3>안녕하세요?</h3>
+      <TourList />
+    </div>
+  );
+}
+
+export default Main;
