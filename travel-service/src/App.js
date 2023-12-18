3d3e35efb2a75280868b67b76012cb2d56ebf1dc
josuhun <chosh9128@gmail.com>
2023-12-18 15:50:53 +0900

    react-TourList

--- travel-service/src/App.js
+++ travel-service/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import TourList from './components/TourList';
 
 function App() {
   return (
     <div className="App">
       <h3>안녕하세요?</h3>
+      <TourList />
     </div>
   );
 }
